3df16fe4ee5d165af9ba96c9905a06d2925d8df7
Sergey <kuzzz99@gmail.com>
2024-03-29 18:54:25 +0000

    Simple solution

---
+++ src/city_data.rs
@@ -0,0 +1,60 @@
+pub struct CityData {
+    pub min: f64,
+    pub max: f64,
+    sum: f64,
+    count: usize,
+}
+
+impl Default for CityData {
+    fn default() -> Self {
+        CityData {
+            min: f64::INFINITY,
+            max: f64::NEG_INFINITY,
+            sum: f64::default(),
+            count: usize::default(),
+        }
+    }
+}
+
+impl CityData {
+    pub fn add(&mut self, num: f64) {
+        self.min = self.min.min(num);
+        self.max = self.max.max(num);
+        self.sum += num;
+        self.count += 1;
+    }
+    pub fn mean(&self) -> f64 {
+        self.sum / self.count as f64
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::CityData;
+
+    fn expect_near(a: f64, b: f64) {
+        const PRECISION: f64 = 1e-9;
+        assert!((a - b).abs() <= PRECISION, "{a} is different from {b}");
+    }
+
+    #[test]
+    fn add_test() {
+        let mut city_data = CityData::default();
+
+        city_data.add(1.);
+        expect_near(city_data.min, 1.);
+        expect_near(city_data.max, 1.);
+        expect_near(city_data.sum, 1.);
+        assert_eq!(city_data.count, 1);
+        expect_near(city_data.mean(), 1.);
+
+        city_data.add(42.);
+        expect_near(city_data.min, 1.);
+        expect_near(city_data.max, 42.);
+        expect_near(city_data.sum, 43.);
+        assert_eq!(city_data.count, 2);
+        expect_near(city_data.mean(), 21.5);
+
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+mod city_data;
+
+use city_data::CityData;
+
+use std::collections::BTreeMap;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::time::SystemTime;
+
 fn main() {
-    println!("Hello, world!");
+    let start_time = SystemTime::now();
+    let file = File::open("./measurements.txt").unwrap();
+    let reader = io::BufReader::new(file);
+
+    let mut cities_data = BTreeMap::<String, CityData>::new();
+
+    for line in reader.lines().flatten().take(1000) {
+        if line.starts_with('#') {
+            continue;
+        }
+        if let Some((city, temperature)) = line.split_once(';') {
+            let temperature: f64 = temperature.parse().unwrap();
+            cities_data.entry(city.to_string()).or_default().add(temperature);
+        }
+    }
+    let processing_duration = SystemTime::now().duration_since(start_time).unwrap();
+
+    for (city, data) in cities_data.iter() {
+        println!("{city}: {:.1}/{:.1}/{:.1}", data.min, data.mean(), data.max);
+    }
+
+    let total_duration = SystemTime::now().duration_since(start_time).unwrap();
+    println!("Processing duration: {} ms", processing_duration.as_millis());
+    println!("Total duration: {} ms", total_duration.as_millis());
 }
