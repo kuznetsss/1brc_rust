bbfb6bdb200e551eb6b60a0f073eb1dd588397bf
Sergey <kuzzz99@gmail.com>
2024-03-29 19:15:02 +0000

    Reuse reading buffer

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,29 @@ use std::time::SystemTime;
 fn main() {
     let start_time = SystemTime::now();
     let file = File::open("./measurements.txt").unwrap();
-    let reader = io::BufReader::new(file);
+    let mut reader = io::BufReader::new(file);
 
     let mut cities_data = BTreeMap::<String, CityData>::new();
 
-    for line in reader.lines().flatten().take(1000) {
+    let mut line = String::with_capacity(1024);
+
+    while reader.read_line(&mut line).unwrap() > 0 {
         if line.starts_with('#') {
+            line.clear();
             continue;
         }
         if let Some((city, temperature)) = line.split_once(';') {
-            let temperature: f64 = temperature.parse().unwrap();
-            cities_data.entry(city.to_string()).or_default().add(temperature);
+            let temperature: f64 = temperature
+                .trim()
+                .parse()
+                .map_err(|_| format!("Can't parse {temperature}"))
+                .unwrap();
+            cities_data
+                .entry(city.to_string())
+                .or_default()
+                .add(temperature);
         }
+        line.clear()
     }
     let processing_duration = SystemTime::now().duration_since(start_time).unwrap();
 
@@ -30,6 +41,9 @@ fn main() {
     }
 
     let total_duration = SystemTime::now().duration_since(start_time).unwrap();
-    println!("Processing duration: {} ms", processing_duration.as_millis());
+    println!(
+        "Processing duration: {} ms",
+        processing_duration.as_millis()
+    );
     println!("Total duration: {} ms", total_duration.as_millis());
 }
